4aebebd8b8f829f7bb28e38bdb346b520bf4840f
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2024-05-08 22:28:27 +0530

    Update Relative Ranks.java

--- DCC-MAY-2024/Relative Ranks.java
+++ DCC-MAY-2024/Relative Ranks.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        String []ans = new String[score.length];
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0; i<score.length; i++){
+            hm.put(score[i],i);
+        }
+        for(int i=0; i< score.length; i++){
+            score[i] = score[i]*-1;
+        }
+        Arrays.sort(score);
+        for(int i=0; i< score.length; i++){
+            score[i] = score[i]*-1;
+        }
+        if(score.length == 1){
+            ans[hm.get(score[0])] = "Gold Medal";
+        }else if(score.length == 2){
+            ans[hm.get(score[0])] = "Gold Medal";
+            ans[hm.get(score[1])] = "Silver Medal";
+        }else{
+            ans[hm.get(score[0])] = "Gold Medal";
+            ans[hm.get(score[1])] = "Silver Medal";
+            ans[hm.get(score[2])] = "Bronze Medal";
+        }
+        
+        for(int i=3; i<score.length; i++){
+            ans[hm.get(score[i])]= ""+ (i+1); 
+        }
+        return ans;
+    }
+}
