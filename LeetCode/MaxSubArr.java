877b8c13aa4b2d029a1c0c7f6c9f2487956b77f1
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2024-06-28 11:32:39 +0530

    Update MaxSubArr.java

--- LeetCode/MaxSubArr.java
+++ LeetCode/MaxSubArr.java
@@ -1,1 +1,24 @@
+class MaxSubArr {
+    public int maxSubArray(int[] nums) {
 
+      int curr_sum = nums[0];
+      int max_so_far = nums[0];
+
+      for(int i = 1; i < nums.length; i++){
+
+         if(curr_sum < 0)
+
+            curr_sum = 0;          
+     
+        curr_sum = curr_sum + nums[i];
+
+        if (curr_sum > max_so_far){
+
+            max_so_far = curr_sum;
+        }
+      }  
+      
+      return max_so_far;     
+    
+    }
+}
