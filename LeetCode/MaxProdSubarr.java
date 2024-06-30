d7ae94029ce853668d7f64ff0c9231f6b42387ed
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2024-06-30 14:49:48 +0530

    Create MaxProdSubarr.java

---
+++ LeetCode/MaxProdSubarr.java
@@ -0,0 +1,38 @@
+class MaxProdSubarr {
+    public int maxProduct(int[] nums) {
+        
+        int minProduct = nums[0];
+        int maxProduct = nums[0];
+        int result = nums[0];
+
+        if(nums.length == 0){
+            
+            return 0;
+            
+        }     
+        for(int i = 1; i < nums.length; i++){
+            
+          if(nums[i] >= 0){
+      
+            maxProduct = Math.max(nums[i] , maxProduct * nums[i]);
+            minProduct = Math.min(nums[i] , minProduct * nums[i]);
+          }
+          else{
+            
+            int tempProd = maxProduct;
+            maxProduct = Math.max(nums[i] , minProduct * nums[i]);
+            minProduct = Math.min(nums[i] , tempProd * nums[i]); 
+          } 
+          
+          result = Math.max(result , maxProduct);
+        
+        }
+        
+        return result;
+    
+    }
+}
+
+
+
+
